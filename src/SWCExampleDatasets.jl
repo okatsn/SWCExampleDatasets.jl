367530c79c90a7e5495fa10ca1fde08010e9d84f
okatsn <okatsn@gmail.com>
2023-03-29 14:00:25 +0800

    Example datasets from SWCDatasets

--- src/SWCExampleDatasets.jl
+++ src/SWCExampleDatasets.jl
@@ -1,5 +1,13 @@
 module SWCExampleDatasets
 
-# Write your package code here.
+
+using SmallDatasetMaker # (required) See also `SmallDatasetMaker.datasets`.
+function SWCExampleDatasets.dataset(package_name, dataset_name)
+    SmallDatasetMaker.dataset(SWCExampleDatasets,package_name, dataset_name)
+end # (optional)
+
+SWCExampleDatasets.datasets() = SmallDatasetMaker.datasets(SWCExampleDatasets) # (optional)
+# so that you can use `SWCExampleDatasets.datasets()` to list all availabe `package/dataest`s in `SWCExampleDatasets`
+
 
 end
