19284bad875e47457e4c472bc968ed2f6ab0439b
Documenter.jl <documenter@juliadocs.github.io>
2023-03-29 05:57:57 +0000

    build based on 367530c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SWCExampleDatasets","category":"page"},{"location":"#SWCExampleDatasets","page":"Home","title":"SWCExampleDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SWCExampleDatasets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SWCExampleDatasets]","category":"page"}]
+}
