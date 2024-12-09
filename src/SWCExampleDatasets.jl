f6ac63928c19346ca5838574a986808abdb392c5
okatsn <okatsn@gmail.com>
2024-12-09 08:21:29 +0000

    Fix SWCExampleDatasets

--- src/SWCExampleDatasets.jl
+++ src/SWCExampleDatasets.jl
@@ -2,11 +2,11 @@ module SWCExampleDatasets
 
 
 using SmallDatasetMaker # (required) See also `SmallDatasetMaker.datasets`.
-function SWCExampleDatasets.dataset(package_name, dataset_name)
-    SmallDatasetMaker.dataset(SWCExampleDatasets,package_name, dataset_name)
+function dataset(package_name, dataset_name)
+    SmallDatasetMaker.dataset(SWCExampleDatasets, package_name, dataset_name)
 end # (optional)
 
-SWCExampleDatasets.datasets() = SmallDatasetMaker.datasets(SWCExampleDatasets) # (optional)
+datasets() = SmallDatasetMaker.datasets(SWCExampleDatasets) # (optional)
 # so that you can use `SWCExampleDatasets.datasets()` to list all availabe `package/dataest`s in `SWCExampleDatasets`
 
 
